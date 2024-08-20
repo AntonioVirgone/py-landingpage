84fa71ce4da69514d8a5bd650a2e7dda46e17126
antonio <antonio.virgone@telepass.com>
2024-08-20 16:47:02 +0200

    create view and model in app landingpage

--- landingpage/models.py
+++ landingpage/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Landingpage(models.Model):
+    title = models.CharField(max_length=255)
+    message = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
