13987a029420cbc464ca0461c2d244b403d5e829
antonio <antonio.virgone@telepass.com>
2024-08-21 11:04:35 +0200

    use context to load data model in template

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Landingpage
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'landingpage/index.html')
+    landingpageList = Landingpage.objects.all()
+    print(landingpageList[0].message)
+
+    for landingpage in landingpageList:
+        print(landingpage.message)
+
+    # Modo uno per passare valori al template
+    context = {}
+    context['title'] = landingpageList[0].title
+    context['message'] = landingpageList[0].message
+
+    return render(request, 'landingpage/index.html', context)
 
 
 def home(request):
-    return HttpResponse("Home Page")
+    landingpageList = Landingpage.objects.all()
+
+    # Modo due per passare valori al template
+    context = { "landingpage" : landingpageList[1] }
+
+    return render(request, 'landingpage/home.html', context)
