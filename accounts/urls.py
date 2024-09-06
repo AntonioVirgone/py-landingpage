4156c65e620d7236166cd40a86c7978f157c7980
antonio <antonio.virgone@telepass.com>
2024-09-06 10:44:39 +0200

    add password change

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 
 from . import views
@@ -11,7 +11,7 @@ urlpatterns = [
          name="logout"),
     path(
         "password_change/",
-        auth_views.PasswordChangeView.as_view(),
+        auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('accounts:password_change_done')),
         name="password_change",
     ),
     path(
